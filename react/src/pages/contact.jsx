d723b7095d43edc7c488d89803ed11376193e0d8
Patrick Riley <righthandriley32@gmail.com>
2025-12-01 14:10:24 -0500

    contact page update

--- react/src/pages/contact.jsx
+++ react/src/pages/contact.jsx
@@ -10,6 +10,13 @@ function Contact() {
         <div className="contactContent">
             <div className="contactDiv">
                 <h2 className="contactTitle">Contact Us!</h2>
+                <h4 className="companyName">Riley Mortgage Consulting Services, LLC</h4>
+                <h4>13430 Parker Commons Blvd. Unit 104</h4>
+                <h4>Fort Myers, FL 33912</h4>
+                <div className="linkedinDiv">
+                    <img src="linkedin.png" alt="" className="linkedinLogo" onClick={() => window.open("https://www.linkedin.com/in/scott-riley-61b3925/", "_blank")} />
+                    <h4 onClick={() => window.open("https://www.linkedin.com/in/scott-riley-61b3925/", "_blank")} className="linkedinText">Connect on Linkedin!</h4>
+                </div>
                 <div className="inputDiv">
                     <div>
                         <h4>First Name:</h4>
